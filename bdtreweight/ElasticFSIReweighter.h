46c847b31b76e8ca8c5e4386d2877454f9bc3a67
zihaolin2000 <zlin22@ur.rochester.edu>
2026-01-26 15:05:02 -0600

    Add new reweighter: ElasticFSIReweighter, to reweight 1p0n FSI elastic fate events to 0.

---
+++ bdtreweight/ElasticFSIReweighter.h
@@ -0,0 +1,52 @@
+// File: ElasticFSIReweighter.h
+// Brief: Boosted-Decision-Tree reweighter that reweights MINERvA medium energy
+// CCQE-like numu-Carbon events
+// from source MC
+//    GENIE v2.12.6
+// to target MC
+//    GENIE 3.0.4 AR23.
+// Author: Zihao Lin zlin22@ur.rochester.edu
+
+#ifndef PLOTUTILS_ELASTICFSIREWEIGHTER_H
+#define PLOTUTILS_ELASTICFSIREWEIGHTER_H
+
+// //PlotUtils includes
+// #include "utilities/NSFDefaults.h"
+// #include "universes/GenieSystematics.cxx" //IsNonResPi()
+// #include "universes/MnvTuneSystematics.cxx" //IsCCRes()
+
+// Vector class to pass event features
+#include <vector>
+
+// Reweighter includes
+#include "weighters/Reweighter.h"
+
+
+namespace PlotUtils
+{
+    template <class UNIVERSE, class EVENT = PlotUtils::detail::empty>
+    class ElasticFSIReweighter: public Reweighter<UNIVERSE, EVENT>
+    {
+        public:
+            ElasticFSIReweighter(): Reweighter<UNIVERSE, EVENT> ()
+            {
+            }
+
+            virtual ~ElasticFSIReweighter() = default;
+
+            double GetWeight(const UNIVERSE& univ, const EVENT& /*event*/) const override
+            {
+                if(univ.GetIsElasticFSIBugFate())
+                    return 0.0;
+                else
+                    return 1.0;
+            }
+
+            std::string GetName() const override { return "Elastic FSI bug reweight"; }
+
+            bool DependsReco() const override { return false; }
+
+    };
+}
+
+#endif //PLOTUTILS_ELASTICFSIREWEIGHTER_H
